24ca5ef7ed52a172b745fd8be47b08c0c60b7ba8
Rushikesh Padaval <143233718+RushikeshPadaval@users.noreply.github.com>
2025-04-16 16:30:52 +0530

    Update mobile-nav.tsx

--- components/nav/mobile-nav.tsx
+++ components/nav/mobile-nav.tsx
@@ -90,22 +90,7 @@ export default function MobileNav({
               </Link>
             ))}
           </nav>
-          <div className="mt-auto">
-            <Card>
-              <CardHeader>
-                <CardTitle>Upgrade to Pro</CardTitle>
-                <CardDescription>
-                  Unlock all features and get unlimited access to our support
-                  team.
-                </CardDescription>
-              </CardHeader>
-              <CardContent>
-                <Button size="sm" className="w-full">
-                  Upgrade
-                </Button>
-              </CardContent>
-            </Card>
-          </div>
+         
         </SheetContent>
       </Sheet>
       <div className="flex items-center md:justify-between w-full gap-1 md:gap-2 py-2">
